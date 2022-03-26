09fcc1292120c5a38e158b8f313872c004dc68b2
montalvas <lucas_montalvani@hotmail.com>
2022-03-26 13:32:03 -0300

    primeiros templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
+class BlogListView(ListView):
+    template_name = 'blog/home.html'
+    model = Post
+    context_object_name = 'posts'
