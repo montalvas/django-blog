f7eef136519bcd278362afd1994837a52e14bc01
montalvas <lucas_montalvani@hotmail.com>
2022-04-07 11:20:50 -0300

    config to deploy

--- main/settings.py
+++ main/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
